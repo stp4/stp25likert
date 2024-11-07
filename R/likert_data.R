e5bb97950f1da07efa7a926b9ec056c05bd56e58
Wolfgang Peter <w.peter@statistik-peter.at>
2024-11-07 10:15:34 +0100

    Leicht Ueberarbeitet

--- R/likert_data.R
+++ R/likert_data.R
@@ -1,96 +1,43 @@
-#' likert data
-#'
-#'  You could use `gglikert_data()` to just produce the dataset to be plotted.
+#' Summary likert data
+#' 
+#' Summe der Items als Long-Datafile.
+#' 
+#' You could use `gglikert_data()` to just produce the dataset to be plotted.
 #'
 #' @param x Tbll_likert -Objekt
 #' @param ... platzhalter
-#' @param include.total margin an Summarise
-#' @param  reverse.levels = FALSE, levels umdrehen
+#' @param reverse.levels logical.  levels umdrehen
+#' @param grouping list. 
+#' @param include.order logical.
+#' @param decreasing logical.
+#' @param use.level logical.
 #'
 #' @return long tibble mit Item, levels,  Freq
 #' @export
 #'
 #' @examples
-#'
-#' #' require(stp25likert)
-#' set.seed(1)
-#' n <- 100
-#' lvs <- c("--", "-", "o", "+", "++")
-#' DF2 <- data.frame(
-#'   Magazines = cut(rnorm(n), 5, lvs),
-#'   Comic.books = cut(rnorm(n), 5, lvs),
-#'   Fiction = cut(rnorm(n), 5, lvs),
-#'   Newspapers = cut(rnorm(n), 5, lvs),
-#'   Geschlecht = cut(rnorm(n), 2, c("m", "f"))
-#' ) |>  stp25tools::Label(Comic.books = "Comic-books")
-#'
-#' dat_lg <- Tbll_likert(
-#'   DF2,
-#'   Magazines,
-#'   Comic.books,
-#'   Fiction,
-#'   Newspapers,
-#'   by =  ~ Geschlecht,
-#'   include.order = TRUE
-#' )
-#'
-#' likert_data(dat_lg)
-#'
-#' likert_data(
-#'   DF2,
-#'   Magazines,
-#'   Comic.books,
-#'   Fiction,
-#'   Newspapers,
-#'   by =  ~ Geschlecht,
-#'   include.total =TRUE
-#' )
-#'
-#' lvls <- c(TRUE, FALSE)
-#' n<- 150
-#' DF_multi <-
-#'   tibble(
-#'     q1 =  rbinom(n, 1, .5) ,
-#'     q2 =   rbinom(n, 1, .4),
-#'     q3 =   rbinom(n, 1, .55),
-#'     q4 =   rbinom(n, 1, .56),
-#'     q5 =   rbinom(n, 1, .53),
-#'     q6 =   rbinom(n, 1, .2),
-#'     q7 =   rbinom(n, 1, .85),
-#'     q8 =  rbinom(n, 1, .95),
-#'     q9 =  rbinom(n, 1, .25)
-#'   ) |>
-#'   mutate(across(everything(), ~ factor(.x ))) |>
-#'   mutate(Sex = sample(c("male", "female"), n, replace = TRUE),
-#'          Age = sample(c("18-30", "30-50", ">50"), n, replace = TRUE)) |>
-#'   stp25tools::Label(
-#'     # FC.2
-#'     q1 =  "Nuts.Seeds",
-#'     q2 =  "Legumes",
-#'     # FC.3
-#'     q3 = "Vegetables Juice ",
-#'     q4 = "Fruit",
-#'     q5 = "Vegetables",
-#'     #   FC.4
-#'     q6 = "Milk",
-#'     q7 = "Cheese",
-#'     q8 = "Yoghurt",
-#'     q9 = "Dairy Alternatives"
+#'  
+#' DF <- dummy_likert_data()
+#' 
+#' rslt <- DF  |> 
+#'   Tbll_likert(
+#'     q1,q2,q3,q4,q5,q6,
+#'     by = ~ sex,
+#'     include.order = TRUE
 #'   )
-#'
-#' summary(DF_multi)
-#'
-#' DF_multi |>
-#'   likert_data(q1,q2, q3, q4, q5, q6, q7, q8, q9,
-#'               by=~ Sex + Age,
-#'               grouping= list(
+#' Summarise_likert(rslt)
+#' 
+#' DF |> 
+#'   Summarise_likert(q1, q2, q3, q4, q5, q6, q7, q8, q9,
+#'               by =~ sex + age,
+#'               grouping = list(
 #'                 FC.2 = c("q1", "q2"),
 #'                 FC.3 = c("q4", "q5", "q6"),
 #'                 FC.4 = c("q7", "q8", "q9")
 #'               )
 #'   )
-#'
-likert_data <- function(x,
+Summarise_likert <- 
+  function(x,
                         ...,
                         grouping = NULL,
                         include.order = TRUE,
@@ -168,13 +115,37 @@ likert_data <- function(x,
   }
 
   datl
-}
+  }
+
+
 #' @export
-#' @rdname likert_data
-multi_data <- function(...,
-                       use.level = 1,  reverse.levels = FALSE) {
-  likert_data(...,  use.level = use.level,  reverse.levels = reverse.levels)
+#' @rdname Summarise_likert
+Summarise_likert_wide <- function(...){
+  levels<- Freq <- .grouping <- Item <- NULL
+  long_df <- Summarise_likert(...)
+  
+  if( any( names( long_df ) == ".grouping"))
+    long_df[,-1] |>  
+    tidyr::spread(levels , Freq ) |>
+    dplyr::mutate(#.grouping =as.character(.grouping),
+      Item =as.character(Item)
+    ) |>
+    dplyr::arrange(.grouping)
+  else long_df |>     
+    tidyr::spread(levels , Freq )
+  
 }
+
+#' @export
+#' @rdname Summarise_likert
+Summarise_multi <-
+  function(...,
+           use.level = 1,
+           reverse.levels = FALSE) {
+    Summarise_likert(..., 
+                     use.level = use.level, 
+                     reverse.levels = reverse.levels)
+  }
 
 likert_data_sum  <-function(...,
                             include.total = FALSE,
@@ -218,110 +189,3 @@ likert_data_sum  <-function(...,
   )
 }
 
-# require(tidyverse)
-# set.seed(42)
-# n <- 243
-# lvls <- c(
-#   "Strongly disagree",
-#   "Disagree",
-#   "Neither agree nor disagree",
-#   "Agree",
-#   "Strongly agree"
-# )
-#
-# DF_likert <-
-#   tibble(
-#     q1 = sample(lvls, n, replace = TRUE),
-#     q2 = sample(lvls, n, replace = TRUE, prob = 5:1),
-#     q3 = sample(lvls, n, replace = TRUE, prob = 1:5),
-#     q4 = sample(lvls, n, replace = TRUE, prob = 1:5),
-#     q5 = sample(lvls, n, replace = TRUE, prob = 1:5),
-#     q6 = sample(lvls,n, replace = TRUE, prob = c(1, 0, 1, 1, 0)),
-#     q7 = sample(lvls, n, replace = TRUE, prob = 1:5),
-#     q8 = sample(lvls, n, replace = TRUE, prob = 1:5),
-#     q9 = sample(lvls, n, replace = TRUE, prob = 1:5)
-#   ) |>
-#   mutate(across(everything(), ~ factor(.x, levels = lvls))) |>
-#   mutate(Sex = sample(c("male", "female"), n, replace = TRUE),
-#          Age = sample(c("18-30", "30-50", ">50"), n, replace = TRUE),
-#          Sex =factor(Sex),
-#          Age =factor(Age)
-#
-#          ) |>
-#   stp25tools::Label(
-#     # FC.2
-#     q1 =  "Nuts.Seeds",
-#     q2 =  "Legumes",
-#     # FC.3
-#     q3 = "Vegetables Juice ",
-#     q4 = "Fruit",
-#     q5 = "Vegetables",
-#     #   FC.4
-#     q6 = "Milk",
-#     q7 = "Cheese",
-#     q8 = "Yoghurt",
-#     q9 = "Dairy Alternatives"
-#   )
-#
-#
-#
-# DF_multi <-
-#   tibble(
-#     q1 = rbinom(n, 1, .50) ,
-#     q2 = rbinom(n, 1, .40),
-#     q3 = rbinom(n, 1, .55),
-#     q4 = rbinom(n, 1, .56),
-#     q5 = rbinom(n, 1, .53),
-#     q6 = rbinom(n, 1, .20),
-#     q7 = rbinom(n, 1, .85),
-#     q8 = rbinom(n, 1, .95),
-#     q9 = rbinom(n, 1, .25)
-#   ) |>
-#   # mutate(across(everything(), ~ factor(.x ))) |>
-#   mutate(Sex = sample(c("male", "female"), n, replace = TRUE),
-#          Age = sample(c("18-30", "30-50", ">50"), n, replace = TRUE),
-#          Sex =factor(Sex),
-#          Age =factor(Age)
-#
-#          ) |>
-#   stp25tools::Label(
-#     # FC.2
-#     q1 =  "Nuts.Seeds",
-#     q2 =  "Legumes",
-#     # FC.3
-#     q3 = "Vegetables Juice ",
-#     q4 = "Fruit",
-#     q5 = "Vegetables",
-#     #   FC.4
-#     q6 = "Milk",
-#     q7 = "Cheese",
-#     q8 = "Yoghurt",
-#     q9 = "Dairy Alternatives"
-#   )
-#
-# x<-
-# DF_likert |>
-#   likert_data(
-#     q1,q2,q3,q4,q5,q6,q7,q8,q9,
-#     by =~ Sex,
-#     grouping = list(
-#       FC.2 = c("q1", "q2"),
-#       FC.3 = "q3",
-#       FC.4 =  c("q4", "q5", "q6"),
-#       FC.5 = c("q7", "q8", "q9")
-#     ),
-#     include.order = TRUE
-#   )
-#
-# DF_likert |>
-#   likert_data(
-#     q1,q2,q3,q4,q5,q6,q7,q8,q9,
-#
-#     grouping = list(
-#       FC.2 = c("q1", "q2"),
-#       FC.3 = "q3",
-#       FC.4 =  c("q4", "q5", "q6"),
-#       FC.5 = c("q7", "q8", "q9")
-#     ),
-#     include.order = TRUE
-#   )
